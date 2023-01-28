4012fcd4f9534693bbd1cb0e5d8f586d71cd7746
~ck~ <chetan.khoche2021@vitbhopal.ac.in>
2023-01-29 01:12:48 +0530

    updated

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.pageX + "px";
   cursor.style.top = e.pageY + "px";
 });
+
+// Adding vanilla tilt js for tilt animation
+VanillaTilt.init(document.querySelector(".card"), {
+  max: 100,
+  speed: 200,
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".card"));
